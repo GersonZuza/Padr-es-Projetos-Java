b35977528a78a6790d3b1d664c7914f7272b75f7
GersonZuza <gersonzuza@gmail.com>
2024-07-16 16:52:44 -0300

    Padrões de Projetos Java - Facade

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
+import one.digitalinnovation.gof.facade.Facade;
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
 import one.digitalinnovation.gof.strategy.*;
+
+
+import javax.xml.stream.FactoryConfigurationError;
 
 public class Test {
     public static void main(String[] args) {
@@ -39,5 +43,10 @@ public class Test {
         robo.mover();
         robo.mover();
         robo.mover();
+
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Gerson", "03809090");
     }
 }
